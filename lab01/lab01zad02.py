7e05f48451c77c3483400b19dd0a89d93d1976ac
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-02-23 20:16:36 +0100

    lab01zad02

--- lab01/lab01zad02.py
+++ lab01/lab01zad02.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 
 miasta = pd.read_csv("~/Documents/inteligencja-obliczeniowa/lab01/miasta.csv")
 
+print("Tabela z danymi: ")
 print(miasta)
 
+print("Wartości tabeli: ")
 print(miasta.values)
 
 miasta.loc[len(miasta)] = [2010, 460, 555, 405]
 
+print("Tabela z dodanym wierszem: ")
 print(miasta)
 
 miasta.plot(x="Rok", y="Gdansk", marker="o", color="red", legend=False,
@@ -23,10 +26,25 @@ plt.show()
 df = miasta.drop('Rok', axis=1)
 
 df_stand = (df - df.mean())/df.std()
-df_stand = pd.concat((df_stand, miasta["Rok"]), 1)
+mean_stand = df_stand.mean()
+sd_stand = df_stand.std()
+df_stand["Rok"] = miasta["Rok"]
+
+print("Ustandaryzowane dane: ")
+print(df_stand)
+print("Średnia: ")
+print(mean_stand)
+print("Odchylenie standardowe: ")
+print(sd_stand)
 
 df_norm = (df - df.min())/(df.max()-df.min())
-df_norm = pd.concat((df_norm, miasta["Rok"]), 1)
+min_norm = df_norm.min()
+max_norm = df_norm.max()
+df_norm["Rok"] = miasta["Rok"]
 
+print("Znormalizowane dane:")
 print(df_norm)
-print(df_stand)
+print("Wartość minimalna: ")
+print(min_norm)
+print("Wartość maksymalna: ")
+print(max_norm)
